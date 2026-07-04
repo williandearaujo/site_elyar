d4d57233348c6e38cd81dae83be44bfd99642104
Willian de Araujo <williandearaujo@hotmail.com>
2026-07-04 01:02:54 -0300

    Correção do script de mapa para habilitar filtros de portfolio nas paginas secundarias

--- js/main.js
+++ js/main.js
@@ -213,21 +213,23 @@ jQuery(function($) {'use strict';
 	});
 
 	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
+	if ($('#google-map').length > 0 && typeof google !== 'undefined') {
+		var latitude = $('#google-map').data('latitude');
+		var longitude = $('#google-map').data('longitude');
+		var initialize_map = function() {
+			var myLatlng = new google.maps.LatLng(latitude,longitude);
+			var mapOptions = {
+				zoom: 14,
+				scrollwheel: false,
+				center: myLatlng
+			};
+			var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map
+			});
 		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
+		google.maps.event.addDomListener(window, 'load', initialize_map);
 	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
